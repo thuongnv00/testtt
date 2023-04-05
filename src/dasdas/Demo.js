5098fe6001e01f82523854d6bd9471cbeea42485
thuongnv00 <ngovanthuong12369@gmail.com>
2023-04-05 18:55:20 +0700

    cm

---
+++ src/dasdas/Demo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Demo.css'
+export default function Demo() {
+    return (<div>
+        <button className="button1" >dsadsa</button>
+    </div>)
+}
---
+++ src/folder/Test.js
@@ -0,0 +1,7 @@
+import './Test.css'
+import React from 'react'
+export default function Test() {
+    return (<div>
+        <button className="button1" >Click</button>
+    </div>)
+}
